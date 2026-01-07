918d26f9fb0ea739ee72ae89562132642de16ca6
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-01-07 04:42:46 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const campos = document.querySelectorAll('input, select');
+const botao = document.getElementById('btnContinuar');
+
+function validarCampos() {
+  let valido = true;
+
+  campos.forEach(campo => {
+    if (campo.value.trim() === '') {
+      valido = false;
+    }
+  });
+
+  if (valido) {
+    botao.disabled = false;
+    botao.classList.add('ativo');
+  } else {
+    botao.disabled = true;
+    botao.classList.remove('ativo');
+  }
+}
+
+campos.forEach(campo => {
+  campo.addEventListener('input', validarCampos);
+});
+
+botao.addEventListener('click', () => {
+  if (!botao.disabled) {
+    window.location.href = 'dieta.html';
+  }
+});
