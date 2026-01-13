469fd463551141987977366091b3230141c883e7
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-01-13 20:30:52 -0300

    Update carregando.js

--- js/carregando.js
+++ js/carregando.js
@@ -3,13 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
   const btnContinuar = document.getElementById('p4Continuar');
   if (!btnContinuar) return;
 
+  // começa bloqueado
+  btnContinuar.disabled = true;
+
+  // libera após 7 segundos
+  setTimeout(() => {
+    btnContinuar.disabled = false;
+  }, 7000);
+
+  // redireciona para pagamento
   btnContinuar.addEventListener('click', function () {
-
-    btnContinuar.disabled = true;
-    btnContinuar.classList.add('desativado');
-
+    if (btnContinuar.disabled) return;
     window.location.href = 'pagamento.html';
-
   });
 
 });
