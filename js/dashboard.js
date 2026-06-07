a04246d42b7df4a16caec3967ec7a37ea64d7779
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-06-07 15:33:53 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,23 @@
-.
+function toggleMenu(){
+
+document
+.getElementById("menuPerfil")
+.classList.toggle("ativo");
+
+}
+
+function abrirReceita(){
+
+document
+.getElementById("modalReceita")
+.style.display="flex";
+
+}
+
+function fecharReceita(){
+
+document
+.getElementById("modalReceita")
+.style.display="none";
+
+}
