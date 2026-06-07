7d86ca452a6fb55ef2add5187eb51a63a8fd9397
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-06-07 04:17:36 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,16 +1,95 @@
-const usuario = JSON.parse(
-  localStorage.getItem("nutri_usuario")
+const usuario =
+JSON.parse(
+localStorage.getItem("nutri_usuario")
 );
 
-if(usuario){
-  document.getElementById("boasVindas")
-  .innerText = `Bem-vindo, ${usuario.nome}!`;
+if(!usuario){
+  window.location.href =
+  "login.html";
 }
 
-function entrarAnamnese(){
-  window.location.href = "anamnese.html";
+/* DADOS */
+
+document.getElementById(
+"nomeUsuario"
+).innerText =
+usuario.email || "Paciente";
+
+document.getElementById(
+"planoAtual"
+).innerText =
+usuario.plano || "Plano não encontrado";
+
+document.getElementById(
+"objetivoAtual"
+).innerText =
+usuario.objetivo || "Não definido";
+
+/* MENU PERFIL */
+
+function toggleMenu(){
+
+  document
+  .getElementById("menuPerfil")
+  .classList
+  .toggle("ativo");
+
 }
 
-function sair(){
-  window.location.href = "login.html";
+/* FECHAR MENU AO CLICAR FORA */
+
+document.addEventListener(
+"click",
+function(e){
+
+const menu =
+document.getElementById(
+"menuPerfil"
+);
+
+const avatar =
+document.querySelector(
+".avatar"
+);
+
+if(
+!menu.contains(e.target)
+&&
+e.target !== avatar
+){
+menu.classList.remove(
+"ativo"
+);
 }
+
+}
+);
+
+/* SAIR */
+
+const links =
+document.querySelectorAll(
+".menu-perfil a"
+);
+
+links.forEach(link=>{
+
+if(
+link.innerText.trim() === "Sair"
+){
+
+link.addEventListener(
+"click",
+function(){
+
+localStorage.removeItem(
+"acessoLiberado"
+);
+
+}
+
+);
+
+}
+
+});
