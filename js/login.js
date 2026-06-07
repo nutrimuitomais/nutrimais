52126264885ce50049442f08a2c3c971af6cf66c
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-06-07 04:24:30 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,14 +3,28 @@ function fazerLogin() {
   const senha = document.getElementById("senha").value.trim();
 
   /* LOGIN DE TESTE */
-  if (
-    email === "viniciuscostacorreia2018@gmail.com" &&
-    senha === "978612"
-  ) {
-    alert("Login realizado com sucesso!");
-    window.location.href = "dashboard.html";
-    return;
-  }
+ if (
+  email === "viniciuscostacorreia2018@gmail.com" &&
+  senha === "978612"
+) {
+
+  localStorage.setItem(
+    "nutri_usuario",
+    JSON.stringify({
+      email: email,
+      plano: "MAX Nutri",
+      tipoPlano: "Anual",
+      objetivo: "Emagrecimento"
+    })
+  );
+
+  alert("Login realizado com sucesso!");
+
+  window.location.href =
+  "dashboard.html";
+
+  return;
+}
 
   /* LOGIN DOS USUÁRIOS CADASTRADOS */
   const usuario = JSON.parse(
