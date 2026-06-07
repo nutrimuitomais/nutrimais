0f3540c7bb07950d81f68033e8a786378d19cba5
nutrimuitomais <viniciuscostacorreia2018@gmail.com>
2026-06-07 03:06:05 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,46 +1,34 @@
-function fazerLogin(){
-
-  const email =
-  document.getElementById("email").value;
-
-  const senha =
-  document.getElementById("senha").value;
-
-  const usuario =
-  JSON.parse(
-    localStorage.getItem("usuarioNutri")
-  );
-
-  if(!usuario){
-    alert("Nenhuma conta encontrada.");
-    return;
-  }
-
-  if(
-    usuario.email === email &&
-    usuario.senha === senha
-  ){
-    window.location.href =
-    "anamnese.html";
-  }
-  else{
-    alert("E-mail ou senha incorretos.");
-  }
-
-}
-
 function fazerLogin() {
   const email = document.getElementById("email").value.trim();
   const senha = document.getElementById("senha").value.trim();
 
+  /* LOGIN DE TESTE */
   if (
     email === "viniciuscostacorreia2018@gmail.com" &&
     senha === "978612"
   ) {
     alert("Login realizado com sucesso!");
-    window.location.href = "dashboard.html";
+    window.location.href = "anamnese.html";
+    return;
+  }
+
+  /* LOGIN DOS USUÁRIOS CADASTRADOS */
+  const usuario = JSON.parse(
+    localStorage.getItem("nutri_usuario")
+  );
+
+  if (!usuario) {
+    alert("Nenhuma conta encontrada.");
+    return;
+  }
+
+  if (
+    usuario.email === email &&
+    usuario.senha === senha
+  ) {
+    window.location.href = "anamnese.html";
   } else {
-    alert("E-mail ou senha inválidos.");
+    alert("E-mail ou senha incorretos.");
   }
 }
 
